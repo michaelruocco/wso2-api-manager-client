a57594ec4b0db2cf34f205059df4696842b00f09
Michael Ruocco <michael.ruocco@hotmail.com>
2017-02-22 04:44:43 +0000

    refactors test name to match implementation class

--- src/test/java/uk/co/mruoc/wso2/DefaultApiTest.java
+++ src/test/java/uk/co/mruoc/wso2/DefaultApiTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ApiTest {
+public class DefaultApiTest {
 
     private final DefaultApi api = new DefaultApi();
 
