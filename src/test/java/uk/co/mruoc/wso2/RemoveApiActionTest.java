7a8344a02b1de6bb046a8a6b42731072ef760ec6
Michael Ruocco <michael.ruocco@hotmail.com>
2017-02-28 09:40:50 +0000

    adds remove api action

---
+++ src/test/java/uk/co/mruoc/wso2/RemoveApiActionTest.java
@@ -0,0 +1,55 @@
+package uk.co.mruoc.wso2;
+
+import org.junit.Test;
+import uk.co.mruoc.http.client.FakeHttpClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class RemoveApiActionTest {
+
+    private static final String URL = "remove-api-url";
+
+    private final ResponseLoader responseLoader = new ResponseLoader();
+    private final FakeHttpClient httpClient = new FakeHttpClient();
+    private final RemoveApiUrlBuilder urlBuilder = new StubRemoveApiUrlBuilder(URL);
+    private final RemoveApiAction action = new RemoveApiAction(httpClient, urlBuilder);
+
+    private final SelectApiParams params = mock(SelectApiParams.class);
+
+    @Test(expected = ApiPublisherException.class)
+    public void removeApiShouldThrowExceptionIfNon200Response() {
+        givenWillReturnNon200();
+
+        action.removeApi(params);
+    }
+
+    @Test(expected = ApiPublisherException.class)
+    public void removeApiShouldThrowExceptionOnRemoveApiFailure() {
+        givenWillReturnRemoveApiFailure();
+
+        action.removeApi(params);
+    }
+
+    @Test
+    public void removeApiShouldReturnTrueOnRemoveApiSuccess() {
+        givenWillReturnRemoveApiSuccess();
+
+        assertThat(action.removeApi(params)).isTrue();
+    }
+
+    private void givenWillReturnNon200() {
+        httpClient.cannedResponse(500, "");
+    }
+
+    private void givenWillReturnRemoveApiFailure() {
+        String body = responseLoader.load("remove-api-failure.json");
+        httpClient.cannedResponse(200, body);
+    }
+
+    private void givenWillReturnRemoveApiSuccess() {
+        String body = responseLoader.load("remove-api-success.json");
+        httpClient.cannedResponse(200, body);
+    }
+
+}
