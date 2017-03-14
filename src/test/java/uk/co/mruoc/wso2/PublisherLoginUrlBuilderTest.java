4b03ec35f79684155888c448dbed7359392acd83
Michael Ruocco <michael.ruocco@hotmail.com>
2017-03-14 13:46:10 +0000

    adds api store login url builder

--- src/test/java/uk/co/mruoc/wso2/PublisherLoginUrlBuilderTest.java
+++ src/test/java/uk/co/mruoc/wso2/PublisherLoginUrlBuilderTest.java
@@ -6,19 +6,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LoginUrlBuilderTest {
 
-    private static final String HOST = "https://localhost:9443";
-    private static final String RESOURCE = "/publisher/site/blocks/user/login/ajax/login.jag";
+    private static final String URL = "https://localhost:9443/publisher/site/blocks/user/login/ajax/login.jag";
 
     private static final String LOGIN_QUERY_STRING = "?action=login&username=%s&password=%s";
 
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "pass";
 
-    private final LoginUrlBuilder builder = new LoginUrlBuilder(HOST);
+    private final LoginUrlBuilder builder = new LoginUrlBuilder(URL);
 
     @Test
     public void shouldBuildUrl() {
-        String expectedUrl = HOST + RESOURCE + String.format(LOGIN_QUERY_STRING, USERNAME, PASSWORD);
+        String expectedUrl = URL + String.format(LOGIN_QUERY_STRING, USERNAME, PASSWORD);
         Credentials credentials = new Credentials(USERNAME, PASSWORD);
 
         String result = builder.build(credentials);
