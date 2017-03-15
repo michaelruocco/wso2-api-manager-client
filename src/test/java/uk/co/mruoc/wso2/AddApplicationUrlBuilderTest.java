2f2598a833621ddc19080732c3d335fe4b2cc34b
Michael Ruocco <michael.ruocco@hotmail.com>
2017-03-15 08:52:40 +0000

    adds add application url builder

---
+++ src/test/java/uk/co/mruoc/wso2/AddApplicationUrlBuilderTest.java
@@ -0,0 +1,28 @@
+package uk.co.mruoc.wso2;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class AddApplicationUrlBuilderTest {
+
+    private static final String HOST_URL = "https://localhost:8443";
+    private static final String RESOURCE = "/store/site/blocks/application/application-add/ajax/application-add.jag";
+    private static final String QUERY_STRING = "?queryString=value";
+
+    private final AddApplicationParamsToQueryStringConverter queryStringConverter = mock(AddApplicationParamsToQueryStringConverter.class);
+    private final AddApplicationUrlBuilder builder = new AddApplicationUrlBuilder(HOST_URL, queryStringConverter);
+
+    @Test
+    public void shouldBuildUrl() {
+        AddApplicationParams params = mock(AddApplicationParams.class);
+        given(queryStringConverter.convert(params)).willReturn(QUERY_STRING);
+
+        String url = builder.build(params);
+
+        assertThat(url).isEqualTo(HOST_URL + RESOURCE + QUERY_STRING);
+    }
+
+}
