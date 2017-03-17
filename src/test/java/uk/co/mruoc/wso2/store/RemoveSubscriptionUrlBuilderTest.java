8f24a41136dd7283072decf1c6517b3225c0abf1
Michael Ruocco <michael.ruocco@hotmail.com>
2017-03-17 14:14:23 +0000

    adds remove subscription url builder

---
+++ src/test/java/uk/co/mruoc/wso2/store/RemoveSubscriptionUrlBuilderTest.java
@@ -0,0 +1,28 @@
+package uk.co.mruoc.wso2.store;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class RemoveSubscriptionUrlBuilderTest {
+
+    private static final String HOST_URL = "https://localhost:8443";
+    private static final String RESOURCE = "/store/site/blocks/subscription/subscription-remove/ajax/subscription-remove.jag";
+    private static final String QUERY_STRING = "?queryString=value";
+
+    private final RemoveSubscriptionParamsToQueryStringConverter queryStringConverter = mock(RemoveSubscriptionParamsToQueryStringConverter.class);
+    private final RemoveSubscriptionUrlBuilder builder = new RemoveSubscriptionUrlBuilder(HOST_URL, queryStringConverter);
+
+    @Test
+    public void shouldBuildUrl() {
+        RemoveSubscriptionParams params = mock(RemoveSubscriptionParams.class);
+        given(queryStringConverter.convert(params)).willReturn(QUERY_STRING);
+
+        String url = builder.build(params);
+
+        assertThat(url).isEqualTo(HOST_URL + RESOURCE + QUERY_STRING);
+    }
+
+}
